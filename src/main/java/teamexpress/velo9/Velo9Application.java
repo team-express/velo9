36e7589166724edad9ae4220783df3ed89b3cc74
Jaemin Kim <jaeminkim90@gmail.com>
2022-03-03 11:13:52 +0900

    first commit

---
+++ src/main/java/teamexpress/velo9/Velo9Application.java
@@ -0,0 +1,13 @@
+package teamexpress.velo9;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Velo9Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Velo9Application.class, args);
+	}
+
+}
