95cea77d48d70d1352a54930772a22da092b7715
sonchanwoo <chanchan1116@naver.com>
2022-03-04 16:06:08 +0900

    feat: 소개글 입력이 없으면 본문의 150자가 소개글이 된다.

--- src/main/java/teamexpress/velo9/post/service/PostServiceImpl.java
+++ src/main/java/teamexpress/velo9/post/service/PostServiceImpl.java
@@ -10,14 +10,39 @@ import teamexpress.velo9.post.domain.PostRepository;
 @Service
 @AllArgsConstructor
 public class PostServiceImpl implements PostService {
+	private static final int FIRST_INDEX = 0;
+	private static final int MAX_INTRODUCE_NUM = 150;
+
 	private PostRepository postRepository;
 
 	@Override
 	public Long save(PostDTO postDTO) {
+
+		if (isIntroduceNUll(postDTO)) {
+			createIntroduce(postDTO);
+		}
+
 		Post post = dtoToEntity(postDTO);
 
 		postRepository.save(post);
 
 		return post.getId();
+	}
+
+	private static boolean isIntroduceNUll(PostDTO postDTO) {
+		return postDTO.getIntroduce() == null;
+	}
+
+	private static boolean smallerThanMaxIntroduceNum(PostDTO postDTO) {
+		return postDTO.getContent().length() < MAX_INTRODUCE_NUM;
+	}
+
+	private static void createIntroduce(PostDTO postDTO) {
+		if (smallerThanMaxIntroduceNum(postDTO)) {
+			postDTO.setIntroduce(postDTO.getContent());
+			return;
+		}
+		postDTO.setIntroduce(
+			postDTO.getContent().substring(FIRST_INDEX, MAX_INTRODUCE_NUM));
 	}
 }
