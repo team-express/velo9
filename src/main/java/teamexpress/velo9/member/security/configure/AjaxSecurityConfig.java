bd2122e97bf5f7199c86cda0fadd80a617b3a10e
kimjinwook1 <rlawlsdnr628@naver.com>
2022-03-14 13:30:01 +0900

    feat: 스프링 시큐리티 기본 로그인 구현

--- src/main/java/teamexpress/velo9/member/security/configure/AjaxSecurityConfig.java
+++ src/main/java/teamexpress/velo9/member/security/configure/AjaxSecurityConfig.java
@@ -4,12 +4,22 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.access.AccessDeniedHandler;
+import org.springframework.security.web.authentication.AuthenticationFailureHandler;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import teamexpress.velo9.member.security.common.AjaxLoginAuthenticationEntryPoint;
+import teamexpress.velo9.member.security.handler.AjaxAccessDeniedHandler;
+import teamexpress.velo9.member.security.handler.AjaxAuthenticationFailureHandler;
+import teamexpress.velo9.member.security.handler.AjaxAuthenticationSuccessHandler;
+import teamexpress.velo9.member.security.provider.AjaxAuthenticationProvider;
 
-@RequiredArgsConstructor
 @Configuration
 @Order(0)
 public class AjaxSecurityConfig extends WebSecurityConfigurerAdapter {
@@ -19,14 +29,56 @@ public class AjaxSecurityConfig extends WebSecurityConfigurerAdapter {
 		return PasswordEncoderFactories.createDelegatingPasswordEncoder();
 	}
 
+	@Bean
+	public AuthenticationSuccessHandler ajaxAuthenticationSuccessHandler() {
+		return new AjaxAuthenticationSuccessHandler();
+	}
+
+	@Bean
+	public AuthenticationFailureHandler ajaxAuthenticationFailureHandler() {
+		return new AjaxAuthenticationFailureHandler();
+	}
+
+	@Bean
+	public AuthenticationProvider ajaxAuthenticationProvider() {
+		return new AjaxAuthenticationProvider();
+	}
+
+	@Bean
+	public AccessDeniedHandler ajaxAccessDeniedHandler() {
+		return new AjaxAccessDeniedHandler();
+	}
+
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http
 			.httpBasic().disable()
 			.csrf().disable()
 			.authorizeRequests()
-			.antMatchers("/signup", "/sendMail").permitAll()
+			.antMatchers("/signup", "/sendMail", "/{nickname}/temp").permitAll()
 			.anyRequest().authenticated();
+
+		http
+			.exceptionHandling()
+			.authenticationEntryPoint(new AjaxLoginAuthenticationEntryPoint())
+			.accessDeniedHandler(ajaxAccessDeniedHandler());
+
+		customConfigurerAjax(http);
 	}
+
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) {
+		auth.authenticationProvider(ajaxAuthenticationProvider());
+	}
+
+	private void customConfigurerAjax(HttpSecurity http) throws Exception {
+		http.
+			apply(new AjaxLoginConfigurer<>())
+			.successHandlerAjax(ajaxAuthenticationSuccessHandler())
+			.failureHandlerAjax(ajaxAuthenticationFailureHandler())
+			.setAuthenticationManager(authenticationManagerBean())
+			.loginProcessingUrl("/api/login");
+	}
+
 }
 
