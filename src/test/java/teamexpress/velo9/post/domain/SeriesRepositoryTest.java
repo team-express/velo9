c253e3b36d7082211eeb5c7352e1ee723eb5c661
kimjinwook1 <rlawlsdnr628@naver.com>
2022-03-17 17:21:32 +0900

    Test: 포스트 및 시리즈 리포지토리 테스트

---
+++ src/test/java/teamexpress/velo9/post/domain/SeriesRepositoryTest.java
@@ -0,0 +1,33 @@
+package teamexpress.velo9.post.domain;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Slice;
+import org.springframework.transaction.annotation.Transactional;
+import teamexpress.velo9.post.dto.SeriesDTO;
+
+@SpringBootTest
+@Transactional
+class SeriesRepositoryTest {
+
+	@Autowired
+	private SeriesRepository seriesRepository;
+
+	@Test
+	void findSeries() {
+	    //given
+		String blogTitle = "admin";
+		PageRequest pageRequest = PageRequest.of(0, 5);
+
+		//when
+		Slice<SeriesDTO> findSeries = seriesRepository.findPostBySeriesName(blogTitle, pageRequest);
+
+	    //then
+		assertThat(findSeries.getNumberOfElements()).isEqualTo(5);
+		assertThat(findSeries).extracting("seriesName").contains("series1", "series2", "series3", "series4", "series5");
+	}
+}
