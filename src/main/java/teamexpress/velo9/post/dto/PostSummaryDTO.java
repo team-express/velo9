5eb99aa0b0996c0e4d613ebae0d2dbc578df18fb
kimjinwook1 <rlawlsdnr628@naver.com>
2022-04-01 14:03:22 +0900

    refactor: id 추가

--- src/main/java/teamexpress/velo9/post/dto/PostSummaryDTO.java
+++ src/main/java/teamexpress/velo9/post/dto/PostSummaryDTO.java
@@ -6,11 +6,13 @@ import teamexpress.velo9.post.domain.Post;
 @Data
 public class PostSummaryDTO {
 
+	private Long id;
 	private String title;
 	private String introduce;
 	private PostThumbnailDTO thumbnail;
 
 	public PostSummaryDTO(Post post) {
+		id = post.getId();
 		title = post.getTitle();
 		introduce = post.getIntroduce();
 		thumbnail = new PostThumbnailDTO(post.getPostThumbnail());
