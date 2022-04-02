0513eb47e78faf3086d61fc027976139a5d704e1
sonchanwoo <chanchan1116@naver.com>
2022-04-02 09:47:34 +0900

    test: contentType 부여

--- src/test/java/teamexpress/velo9/post/controller/SeriesControllerTest.java
+++ src/test/java/teamexpress/velo9/post/controller/SeriesControllerTest.java
@@ -15,6 +15,7 @@ import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDoc
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Import;
+import org.springframework.http.MediaType;
 import org.springframework.test.annotation.Rollback;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
@@ -55,7 +56,8 @@ public class SeriesControllerTest {
 				.content("{"
 					+ "\n\"memberId\":1,"
 					+ "\n\"name\":\"loveyou\""
-					+ "\n}"))
+					+ "\n}")
+				.contentType(MediaType.APPLICATION_JSON))
 			.andExpect(status().isOk())
 			.andDo(document("addSeries",
 				requestFields(
