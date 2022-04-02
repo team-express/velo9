2cda21f54d150ae49be0d8681b82e204ad45884f
sonchanwoo <chanchan1116@naver.com>
2022-04-02 09:37:57 +0900

    test: 시리즈 컨트롤러 테스트 및 문서화 완료

--- src/main/java/teamexpress/velo9/post/dto/SeriesReadDTO.java
+++ src/main/java/teamexpress/velo9/post/dto/SeriesReadDTO.java
@@ -10,11 +10,9 @@ public class SeriesReadDTO {
 
 	private Long id;
 	private String name;
-	private Long memberId;
 
 	public SeriesReadDTO(Series series) {
 		this.id = series.getId();
 		this.name = series.getName();
-		this.memberId = series.getMember().getId();
 	}
 }
