feb390dd263ed528801c20418bc39cd8c11e469a
kimjinwook1 <rlawlsdnr628@naver.com>
2022-04-16 21:25:15 +0900

    refactor: 생성자 제거

--- src/main/java/teamexpress/velo9/member/domain/Look.java
+++ src/main/java/teamexpress/velo9/member/domain/Look.java
@@ -33,9 +33,4 @@ public class Look {
 	@ManyToOne(fetch = FetchType.LAZY)
 	@JoinColumn(name = "member_id")
 	private Member member;
-
-	public Look(Post post, Member member) {
-		this.post = post;
-		this.member = member;
-	}
 }
